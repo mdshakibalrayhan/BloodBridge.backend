02598be9ca144b961780bf42cb3bbd8f0955bc4b
mdshakibalrayhan <rayhanshakib14@gmail.com>
2025-02-14 14:56:49 +0600

    bugs fixing

--- BloodBridge/settings.py
+++ BloodBridge/settings.py
@@ -81,6 +81,12 @@ INSTALLED_APPS = [
 # Django REST Framework
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 # Templates
