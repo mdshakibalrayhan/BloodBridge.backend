c020a657dc2077e09217fa45d0dcd577ae7f479f
mdshakibalrayhan <rayhanshakib14@gmail.com>
2025-02-22 02:17:27 +0600

    donor name added

--- donation/serializers.py
+++ donation/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from .models import Donation
-
+from account.models import UserAccount
 class DonationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Donation
         fields = ['event']
 
 class AllDonationSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(many=False)
+    donor_name = serializers.SerializerMethodField() 
     class Meta:
         model = Donation
-        fields = '__all__'
+        fields = '__all__' 
+
+    def get_donor_name(self, obj):
+        return obj.donor.user_account.first_name  
