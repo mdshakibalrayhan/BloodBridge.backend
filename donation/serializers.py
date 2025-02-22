2566216e5634f371593b750d1768c2cf160129c8
mdshakibalrayhan <rayhanshakib14@gmail.com>
2025-02-22 14:04:00 +0600

    event name added

--- donation/serializers.py
+++ donation/serializers.py
@@ -5,12 +5,17 @@ class DonationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Donation
         fields = ['event']
-
 class AllDonationSerializer(serializers.ModelSerializer):
     donor_name = serializers.SerializerMethodField() 
+    event_name = serializers.SerializerMethodField()  # Add this line
+
     class Meta:
         model = Donation
-        fields = '__all__' 
+        fields = '__all__'  # This will include all fields, including event ID
 
     def get_donor_name(self, obj):
-        return obj.donor.user_account.first_name  
+        return obj.donor.user_account.first_name
+    
+    def get_event_name(self, obj):
+        return obj.event.title  # Assuming your Event model has a 'title' field
+
